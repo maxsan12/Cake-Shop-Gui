dfa9b94ee8a6df2f0d85b6c1a38675ea3999c97b
Maxinne Santico <tnd0299@autuni.ac.nz>
2022-10-15 20:58:00 +1300

    Initial commit

---
+++ src/cake/shop/mvc/Data.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cake.shop.mvc;
+
+/**
+ *
+ * @author Maxinne Santico 19084694
+ * COMP603/50 - Group 27
+ * Assignment 2
+ * 
+ * Data class - needed for 
+ */
+public class Data {
+    
+}
