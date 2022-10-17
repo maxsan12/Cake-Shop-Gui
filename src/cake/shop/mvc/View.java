ccd8a3375ecc229fbf1e8e69d48629217d888684
Maxinne Santico <maxinnesjs@gmail.com>
2022-10-17 16:56:00 +1300

    Initial commit for cake shop

--- src/cake/shop/mvc/View.java
+++ src/cake/shop/mvc/View.java
@@ -4,10 +4,32 @@
  */
 package cake.shop.mvc;
 
+import java.awt.event.ActionListener;
+import java.util.Observable;
+import java.util.Observer;
+import javax.swing.JFrame;
+
 /**
  *
- * @author maxin
+ * @author Maxinne Santico 19084694
+ * COMP603/50 - 19084694
+ * 
+ * This class is designed as the 'View' class inspired by the MVC design pattern
+ * The view class 
  */
-public class View {
+public class View  extends JFrame implements Observer {
     
+    // need to create Action Listener
+    private ActionListener actionListener; 
+    
+
+    @Override
+    public void update(Observable o, Object o1) {
+        
+    }
+    
+    
+    private void mainPageGui() {
+        
+    }
 }
